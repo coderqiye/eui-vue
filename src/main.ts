e288a769be0a333230fb44239d51d17a14792bab
qiye <coderqiye@163.com>
2021-03-26 19:27:56 +0800

    初始化项目

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: qiye
+ * @Date: 2021-03-26 19:01:18
+ * @LastEditors: qiye
+ * @LastEditTime: 2021-03-26 19:09:13
+ * @Description: file content
+ */
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import App from './App.vue'
+
+
+createApp(App).mount('#app')
